43c92f01a0d6a7108996c6295a16290151df5205
JoMaRoNe <jmromero@duck.com>
2024-01-13 22:41:04 +0100

    acabando Part1-c

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,22 +1,43 @@
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hola {props.name}, tienes {props.age} años.
-      </p>
-    </div>
-  )
-}
+import { useState } from "react"
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onSmash, text}) => <button onClick={onSmash}>{text}</button>
 
 const App = () => {
-  const name = 'Pedro'
-  const age = 10
+  const [ counter, setCounter ] = useState(0)
+  console.log('renderizando con valor de contador: ', counter)
+
+  const increaseByOne = () => {
+    console.log('incrementando, valor previo: ', counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decrementando, valor previo: ', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('Reseteando a cero, valor previo: ', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      <h1>Saludos</h1>
-      <Hello name="Maya" age={26+10} />
-      <Hello name={name} age={age} />
+      <Display counter={counter} />
+      <Button 
+        handleClick={increaseByOne}
+        text='Mas'
+      />
+      <Button 
+        handleClick={setToZero}
+        text='Reiniciar'
+      />
+      <Button 
+        handleClick={decreaseByOne}
+        text='Menos'
+      />
     </div>
   )
 }
+
+export default App
