3fc32bcf53ecd392849f2b8e5bd02b5b06a7adc4
JoMaRoNe <jmromero@duck.com>
2024-01-27 17:10:31 +0100

    exercise 2.2 finished

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,10 +1,12 @@
 const Course = ({course}) => {
+    const total =course.parts.reduce((sum,part) => sum + part.exercises ,0)
     return (
         <div>
             <h1>{course.name}</h1>        
             {course.parts.map((part,i) => (
                 <p key={i}>{part.name} {part.exercises}</p>
-            ) )}
+            ))}
+            <p>total of {total} exercises.</p>
         </div>
     )
 }
