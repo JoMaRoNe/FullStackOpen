33568a8cac83d0a5a69517239ab3ac2a75b4d056
JoMaRoNe <jmromero@duck.com>
2024-01-27 16:36:40 +0100

    exercise 2.1 finished

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,16 +1,4 @@
-const Curso = ({course}) => {
-  console.log("curso:", course);
-  return (
-    <div>
-      {course.map((c, i) => (
-        <div>
-          <h1>{c.name}</h1>
-          <p>{c.parts.name} {c.parts.exercises}</p>
-        </div>
-      ))} 
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const course = {
@@ -35,71 +23,7 @@ const App = () => {
     ],
   }
 
-  return <Curso course={course} />
+  return <Course course={course} />
 }
 
 export default App
-
-/*
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part name = {props.course.parts[0].name} exercises = {props.course.parts[0].exercises} />
-      <Part name = {props.course.parts[1].name} exercises = {props.course.parts[1].exercises} />
-      <Part name = {props.course.parts[2].name} exercises = {props.course.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return(
-    <div>
-      <p>Number de exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-    </div>
-  )
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-    {
-      name: 'Fundamentals of React', 
-      exercises: 10 
-    },
-    { 
-      name: 'Using props to pass data', 
-      exercises: 7
-    },
-    { 
-      name: 'State of component', 
-      exercises: 14
-    }
-    ]
-  }
-
-  return (
-    <div>
-      <Header course = {course} />
-      <Content course = {course} />
-      <Total course = {course} />
-    </div>
-  )
-}
-*/
