ae6333f3cf8e978a839f379e8b861cf841c6f262
JoMaRoNe <jmromero@duck.com>
2024-01-27 17:45:02 +0100

    exercises 2.3+2.4+2.5 finished

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,12 +1,17 @@
-const Course = ({course}) => {
-    const total =course.parts.reduce((sum,part) => sum + part.exercises ,0)
+const Course = ({courses}) => {
+
     return (
         <div>
-            <h1>{course.name}</h1>        
-            {course.parts.map((part,i) => (
-                <p key={i}>{part.name} {part.exercises}</p>
-            ))}
-            <p>total of {total} exercises.</p>
+            <h1>Web development curriculum</h1>
+            {courses.map((course,i) => (
+                <div key={i}>
+                    <h2 index={i}>{course.name}</h2>
+                    {course.parts.map((part,i) => (
+                        <p key={i}>{part.name} {part.exercises}</p>
+                    ))}
+                    <p><strong>total of {course.parts.reduce((sum,part) => sum + part.exercises ,0)} exercises.</strong></p>
+                </div>
+            ))}  
         </div>
     )
 }
