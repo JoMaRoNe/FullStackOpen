922bcad53628cfe3a0b1f00d29dc5779f4813c49
JoMaRoNe <jmromero@duck.com>
2024-02-06 21:16:04 +0100

    Exercise 2.8 finished + readme.md

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,22 +3,29 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault ()
     
     persons.some(person => person.name === newName)
       ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat({name:newName}))
+      : setPersons(persons.concat({name:newName, number:newNumber}))
     
     setNewName('')
+    setNewNumber('')
   }
 
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -30,6 +37,12 @@ const App = () => {
             value={newName}
             onChange={handlePersonChange}
           />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
+           />  
         </div>
         <div>
           <button type="submit">add</button>
